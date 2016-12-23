994afaac7644cb66b31c61ec54ea67cfb76f3a79
jehans storvik <jehans storvik>
2016-12-23 13:50:39 +0100

    Click now for mobile and desktop versions

--- js.js
+++ js.js
@@ -4,30 +4,23 @@ var width = $( document ).width();
     
     var svg = document.getElementById("svgFile"); 
     
-    
-    if(width < 1024) {
+    svg.setAttribute("data", "Mobil_vinterkampanje.svg");
+   /* if(width < 1024) {
         svg.setAttribute("data", "Mobil_vinterkampanje.svg");
     } else {
         svg.setAttribute("data", "Vinterkampanje.svg");
-    }
+    }*/
     
 $(window).on('load', function() {
+    var svgDoc = svg.contentDocument;
     
-    
-    var svgDoc;
-    var fossheim;
-        
-    console.log("SetAttribute done");
-        
-    svgDoc = svg.contentDocument;
-    fossheim = svgDoc.getElementById("fossheim");
-    console.log(fossheim);
+    var fossheim = svgDoc.getElementById("fossheim");
+    //var a = svgDoc.getElementById("");
     
     fossheim.addEventListener("mousedown", function () {
                     console.log("Fosshaaaim");
                 }, false);
-
-                
+                      
 });
 
 function clickHandler() {
